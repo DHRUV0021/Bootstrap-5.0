839625433b32e08d6361e50c56e81e8764582095
DHRUV0021 <gorasiyadhruv021@gmail.com>
2023-05-10 22:44:19 +0530

    sum changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AccordionComponent } from './accordion/accordion.component';
+import { AlertsComponent } from './alerts/alerts.component';
+import { BadgeComponent } from './badge/badge.component';
+import { BreadcrumbComponent } from './breadcrumb/breadcrumb.component';
+import { ButtonsComponent } from './buttons/buttons.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AccordionComponent
+    AccordionComponent,
+    AlertsComponent,
+    BadgeComponent,
+    BreadcrumbComponent,
+    ButtonsComponent
   ],
   imports: [
     BrowserModule,
